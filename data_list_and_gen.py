3df7375516ded6558d61de4fb493b04c960d8b01
Dominik Lenart <dl7cz@virginia.edu>
2017-08-16 18:59:43 -0400

    Finished data preproccesing for Kaplan data structure input. Columns with genes that do not vary in their expression are now removed after labeling the samples with their respective quartiles.

--- data_list_and_gen.py
+++ data_list_and_gen.py
@@ -1,11 +1,11 @@
 import os
 from firebrowse import fbget
 import pickle
-
 from pandas import DataFrame
 import pandas as pd
 import lifelines
 from matplotlib import pyplot as plt
+import numpy
 
 CD = os.path.dirname(os.path.abspath(__file__))
 SEQUESTERED_GENES = [i.rstrip() for i in open('pure-sequestered-cta')]
@@ -94,13 +94,17 @@ def check_mRNASeq_data(cohort):
             break
     print 'No problems with mRNASeq in ' + str(cohort)
 
+
 def merge_clinical_with_mrna(cohort):
 
     ''' Order of data:
-    1. Barcode
-    2. Death status (1 = dead, 0 = alive)
-    3. Days to death (NA if alive)
-    4. Days to last followup (NA if dead) '''
+    0. Barcode
+    1. Death status (1 = dead, 0 = alive) - to use in censoring
+    2. Days to death (NA if alive)
+    3. Days to last followup (NA if dead)
+    4. Time to death OR last followup
+    5 - end. Data for each gene
+    '''
 
     os.chdir(str(CD + '/' + cohort))
     if not os.path.isfile(str(CD + '/' + cohort + '/' + 'merged_data_' + cohort)):
@@ -168,32 +172,132 @@ def merge_clinical_with_mrna(cohort):
 
         os.chdir(str(CD + '/' + cohort))
 
-        # Removing patients that have wrong survival data from file
-        for i in relevant_data:
-            if i == relevant_data[0]:
+        # Removing patients that have wrong survival data or no rnaseq from file
+        for line in relevant_data:
+
+            # Skip 1st line
+            if line == relevant_data[0]:
                 pass
             else:
-                if i[4] == 'NA' or int(i[4]) < 0:
-                    relevant_data.pop(relevant_data.index(i))
-                    print "Removed entry"
+                # Remove entries that have wrong survival time data
+                if line[4] == 'NA' or int(line[4]) < 0:
+                    relevant_data.pop(relevant_data.index(line))
+                    print "Removed entry due to wrong lifespan data"
 
+                # For each column with gene data check if they are valid, if not - remove.
+                for i in range(len(relevant_data[0][5:])):
+                    if line[i] == 'Not found':
+                        relevant_data.pop(relevant_data.index(line))
+                        print "Removed entry due to wrong RNASeq lifespan data"
+                        break
+                    else:
+                        pass
         # Dumping a list with relevant data to file
         with open(str('merged_data_' + cohort), 'wb') as fp:
             pickle.dump(relevant_data, fp)
 
-        return 0
     else:
-        with open(str('merged_data_' + cohort), 'rb') as fp:
-            DATA = pickle.load(fp)
-
-        return DATA
+        pass
 
 
-def kaplan_meier_plot_and_stat(data):
+def label_genes_with_quartiles(cohort):
+    os.chdir(str(CD + '/' + cohort))
 
-    for i in data:
+    with open(str('merged_data_' + cohort), 'rb') as fp:
+        data = pickle.load(fp)
+
+    # Make labels for each quartile that will be added as last columns at the rightmost side
+    clinical_labels = [i for i in data[0]]
+    gene_labels = [i + ' Quartile' for i in data[0][5:]]
+
+    # Strip the line with labels
+    data = data[1:]
+
+    # Determine quartiles (25, 75) for each gene expression. Apply quartiles to main data structure
+
+    genes_with_varying_expression = []
+    genes_with_no_changes_in_expression = []
+    dict_gene_name_expression_list = {}
+
+    NEW_DATA_LIST = [clinical_labels + gene_labels] + data
+    to_append_each_line = ['' for i in range(len(SEQUESTERED_GENES))]
+    for i in NEW_DATA_LIST:
+        i += to_append_each_line
+
+    for gene in range(len(SEQUESTERED_GENES)):
+        list_with_gene_expression_score = []
+        for line in data:
+            list_with_gene_expression_score.append(line[gene+5])
+
+        list_with_gene_expression_score = [float(i) for i in list_with_gene_expression_score]
+        dict_key = SEQUESTERED_GENES[gene]
+        dict_gene_name_expression_list[dict_key] = list_with_gene_expression_score
+
+        numpy_array = numpy.array(list_with_gene_expression_score)
+        percentile_50 = numpy.percentile(numpy_array, 50)
+        percentile_75 = numpy.percentile(numpy_array, 75)
+
+        list_of_quartile_determination = []
+        index_of_column_to_edit = NEW_DATA_LIST[0].index(str(dict_key) + ' Quartile')
+
+        # Make labels for each patient
+        for i in list_with_gene_expression_score:
+            if i < percentile_50:
+                list_of_quartile_determination += ['Median']
+            elif i > percentile_75:
+                list_of_quartile_determination += ['Third_quartile']
+            else:
+                list_of_quartile_determination += ['Between_50_and_75']
+
+        for i, determination in enumerate(list_of_quartile_determination):
+            NEW_DATA_LIST[i+1][index_of_column_to_edit] = determination
+
+        if percentile_50 == percentile_75:
+            genes_with_no_changes_in_expression.append(SEQUESTERED_GENES[gene])
+
+        else:
+            genes_with_varying_expression.append(SEQUESTERED_GENES[gene])
+
+    # Need to remove empty elements from the header
+    NEW_DATA_LIST[0] = NEW_DATA_LIST[0][:93]
+
+    print 'Changes in expression in: ' + str(genes_with_varying_expression)
+    print 'No change in expression in: ' + str(genes_with_no_changes_in_expression)
+
+    # Time to remove data for genes that do not change their expression
+    gene_index = []
+    gene_quartile_index = []
+
+    for i in genes_with_no_changes_in_expression:
+        gene_index.append(NEW_DATA_LIST[0].index(i))
+        gene_quartile_index.append(NEW_DATA_LIST[0].index(str(i + ' Quartile')))
+
+    print gene_index
+    print gene_quartile_index
+    indexes_to_remove = gene_index + gene_quartile_index
+
+    # Sorting the list in reverse order, so removing by index removes correct entries
+    indexes_to_remove = sorted(indexes_to_remove, reverse=True)
+    print indexes_to_remove
+    for i in indexes_to_remove:
+        for line in NEW_DATA_LIST:
+            line.pop(i)
+
+    for i in NEW_DATA_LIST[0:5]:
         print i
 
+    # Exporting new data file
+    with open(str('merged_data_with_quartiles_' + cohort), 'wb') as fp:
+        pickle.dump(NEW_DATA_LIST, fp)
+
+def kaplan_meier_plot_and_stat(cohort):
+
+    os.chdir(str(CD + '/' + cohort))
+
+    with open(str('merged_data_' + cohort), 'rb') as fp:
+        data = pickle.load(fp)
+
+    # Loading 1st row of list with data as header
     header = data[0]
     data = data[1:]
     pandas_data = DataFrame(data=data, columns=header)
@@ -213,7 +317,6 @@ def process_data(cohort):
     check_mRNASeq_data(cohort)
 
 
-
-x = merge_clinical_with_mrna('BRCA')
-
-kaplan_meier_plot_and_stat(x)
+merge_clinical_with_mrna('BRCA')
+#kaplan_meier_plot_and_stat('BRCA')
+label_genes_with_quartiles('BRCA')
